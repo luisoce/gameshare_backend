34b022958142031ed95bfb5cb4a6e5a9800f1a9d
luisoce <luis.campos.escobar@gmail.com>
2024-06-27 23:38:41 -0400

    Proyecto backend

---
+++ src/main/java/com/proyectoIntegrador/gameShare/seguridad/DetallesDeUsuarioServicio.java
@@ -0,0 +1,37 @@
+package com.proyectoIntegrador.gameShare.seguridad;
+
+import com.proyectoIntegrador.gameShare.entidad.Rol;
+import com.proyectoIntegrador.gameShare.entidad.Usuario;
+import com.proyectoIntegrador.gameShare.repositorio.UsuarioRepositorio;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class DetallesDeUsuarioServicio implements UserDetailsService {
+    private UsuarioRepositorio usuarioRepositorio;
+
+    // Método para traer lista de autoridades basadas en el rol del usuario.
+    public Collection<GrantedAuthority> mapearAutoridad(Rol rol) {
+        return Collections.singletonList(new SimpleGrantedAuthority(rol.getNombreRol()));
+    }
+
+    // Método para traernos los datos del usuario por medio de su email.
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepositorio.findByEmail(email)
+                .orElseThrow( () -> new UsernameNotFoundException("Email no encontrado."));
+        return new User(usuario.getEmail(), usuario.getContrasenia(), mapearAutoridad(usuario.getRol()));
+    }
+}
---
+++ src/main/java/com/proyectoIntegrador/gameShare/servicio/UsuarioServicio.java
@@ -0,0 +1,92 @@
+package com.proyectoIntegrador.gameShare.servicio;
+
+import com.proyectoIntegrador.gameShare.dto.UsuarioRegistroDTO;
+import com.proyectoIntegrador.gameShare.entidad.Rol;
+import com.proyectoIntegrador.gameShare.entidad.Usuario;
+import com.proyectoIntegrador.gameShare.repositorio.RolRepositorio;
+import com.proyectoIntegrador.gameShare.repositorio.UsuarioRepositorio;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class UsuarioServicio {
+    private UsuarioRepositorio usuarioRepositorio;
+    private PasswordEncoder encriptarContrasenia;
+    private RolRepositorio rolRepositorio;
+    public Usuario registrarUsuario(UsuarioRegistroDTO usuarioDTO) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre(usuarioDTO.getNombre());
+        usuario.setApellido(usuarioDTO.getApellido());
+        usuario.setEmail(usuarioDTO.getEmail());
+        /*
+        usuario.setFechaNacimiento(usuarioDTO.getFechaNacimiento());
+        usuario.setEdad(usuario.calcularEdad(usuarioDTO.getFechaNacimiento()));
+         */
+        usuario.setContrasenia(encriptarContrasenia.encode(usuarioDTO.getContrasenia()));
+
+        Rol rol = rolRepositorio.findByNombreRol("USUARIO").orElse(null);
+        usuario.setRol(rol);
+
+        return usuarioRepositorio.save(usuario);
+    }
+    public Usuario registrarUsuarioAdmin(UsuarioRegistroDTO usuarioDTO) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre(usuarioDTO.getNombre());
+        usuario.setApellido(usuarioDTO.getApellido());
+        usuario.setEmail(usuarioDTO.getEmail());
+        /*
+        usuario.setFechaNacimiento(usuarioDTO.getFechaNacimiento());
+        usuario.setEdad(usuario.calcularEdad(usuarioDTO.getFechaNacimiento()));
+        */
+        usuario.setContrasenia(encriptarContrasenia.encode(usuarioDTO.getContrasenia()));
+
+        Rol rol = rolRepositorio.findByNombreRol("ADMINISTRADOR").orElse(null);
+        usuario.setRol(rol);
+
+        return usuarioRepositorio.save(usuario);
+    }
+    public Optional<Usuario> buscarUsuarioPorID(Long id){
+        return usuarioRepositorio.findById(id);
+    }
+    public Optional<Usuario> buscarUsuarioPorEmail(String email){
+        return usuarioRepositorio.findByEmail(email);
+    }
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepositorio.findAll();
+    }
+    public Usuario actualizarUsuario(Long id, UsuarioRegistroDTO usuarioDTO) {
+        Usuario usuario = usuarioRepositorio.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        usuario.setNombre(usuarioDTO.getNombre());
+        usuario.setApellido(usuarioDTO.getApellido());
+        usuario.setEmail(usuarioDTO.getEmail());
+    /*
+    usuario.setFechaNacimiento(usuarioDTO.getFechaNacimiento());
+    usuario.setEdad(usuario.calcularEdad(usuarioDTO.getFechaNacimiento()));
+    */
+        usuario.setContrasenia(encriptarContrasenia.encode(usuarioDTO.getContrasenia()));
+
+        Rol rol = rolRepositorio.findByNombreRol("USUARIO").orElse(null);
+        usuario.setRol(rol);
+
+        return usuarioRepositorio.save(usuario);
+    }
+
+    public void eliminarUsuario(Long id) { usuarioRepositorio.deleteById(id);}
+}
